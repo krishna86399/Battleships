d11bdba2494a03298f0aaf3b2dc060c914bfe697
krishnavamsi <7krishnavamsi@msitprogram.net>
2021-12-15 16:38:47 +0530

    completed create ship

--- battleship.py
+++ battleship.py
@@ -71,7 +71,25 @@ Parameters: no parameters
 Returns: 2D list of ints
 '''
 def createShip():
-    return
+    import random
+    row = random.randint(1,8)
+    col = random.randint(1,8)
+    hv = random.randint(0,1)
+    if hv==0:
+      p=row-1
+      randomValue=col
+    else:
+      p=col-1
+      randomValue=row
+    d1=[[randomValue for j in range(1)] for i in range(3)]
+
+    for i in range(len(d1)):
+      d2=d1[i]
+ 
+      d2.insert(hv,p)
+      p=p+1
+    
+    return d1
 
 
 '''
@@ -80,6 +98,7 @@ Parameters: 2D list of ints ; 2D list of ints
 Returns: bool
 '''
 def checkShip(grid, ship):
+    
     return
 
 
